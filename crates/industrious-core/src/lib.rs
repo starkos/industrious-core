b6d2964ad4a3a07f94d2f28e46e12f46e41e878c
Jess Perkins <starkos@industriousone.com>
2024-09-05 13:44:20 -0400

    Set up the project structure

---
+++ crates/industrious-core/src/lib.rs
@@ -0,0 +1,2 @@
+// Copyright (c) 2025 Industrious One, LLC
+// SPDX-License-Identifier: MIT
---
+++ examples/counter/src/main.rs
@@ -0,0 +1,6 @@
+// Copyright (c) 2025 Industrious One, LLC
+// SPDX-License-Identifier: MIT
+
+fn main() {
+	println!("A counter example is coming soon");
+}
